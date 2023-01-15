c38bcd90115ed1c37e80114cddc49b5e185c6145
George Kimari <grgkimari@gmail.com>
2023-01-15 23:21:46 +0300

    added timeout sound audio

--- countdown-timer/src/App.js
+++ countdown-timer/src/App.js
@@ -1,15 +1,30 @@
 import './App.css';
 import Clock from './Components/Clock'
-import React, { useReducer, useContext} from 'react'
+import React, { useReducer, useContext, useEffect} from 'react'
 import mainReducer, {COUNT_DOWN, SET_INTERVAL, RESET, CLEAR_INTERVAL, initialState} from './reducer'
 
 export const DispatchContext = React.createContext()
 
+const playAudio = () => {
+  const audio = new Audio('clock-sound.wav')
+
+  audio.play()
+}
+
 function App() {
   const [state, dispatch] = useReducer(mainReducer, initialState)
 
+  useEffect(
+    () => {
+      if(state.timeLeft.hours === 0 && state.timeLeft.minutes === 0 && state.timeLeft.seconds === 0){
+        playAudio()
+    }
+  }, [state.timeLeft.minutes, state.timeLeft.hours, state.timeLeft.seconds]
+  )
+
   return (
     <div className="App">
+    
       <DispatchContext.Provider value={dispatch}>
       <Clock time ={state.timeLeft}/>
 
